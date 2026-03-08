f859dadd8b2e08ee675d4919ddd2b75ffa8f23bc
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-03-08 16:02:12 +0530

    Assignments 2 Validation Of strong password

--- Assi2Q6.py
+++ Assi2Q6.py
@@ -1,1 +1,26 @@
-a=input("Enter your future password: ")
+import string
+
+password = input("Enter a password: ")
+
+upper = False
+lower = False
+digit = False
+symbol = False
+
+if len(password) >= 8:
+    for ch in password:
+        if ch.isupper():
+            upper = True
+        elif ch.islower():
+            lower = True
+        elif ch.isdigit():
+            digit = True
+        elif ch in string.punctuation:
+            symbol = True
+
+    if upper and lower and digit and symbol:
+        print("Strong Password ")
+    else:
+        print("Password is not strong ")
+else:
+    print("Password must be at least 8 characters long ")
