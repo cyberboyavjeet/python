9459458211d3bfe91fd3786731909eea4be87ed9
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-03-14 11:52:18 +0530

    List in Python

---
+++ list.py
@@ -0,0 +1,8 @@
+list1=[1,0,0,2,'hi','',[]]
+print(list(filter(bool.0)))
+#print(list1)
+print(list1[-1])#reverse indexing
+print(list1[1:3])#slicing based rangein list
+print(list1[0:6])#slicing
+print(list1[-10:])
+print(list1[-2:-2])#empty list
